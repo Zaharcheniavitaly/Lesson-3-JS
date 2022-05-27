3ce679c3829fbeb83142a84ffde2c48f4bcd1207
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-05-27 19:33:10 +0300

    update

--- js/script1.js
+++ js/script1.js
@@ -2,15 +2,20 @@
 'use strict'
 
 //Задание 1
+/*
+for (let i = 0; i < 11; i++) {
 
-for (let i = 0; i < 10; i++) {
-
-
-	console.log('Показываем число ' + i);
-
-	if (i = 0) {
-		console.log(i + '- это ноль');
-
+	if (i == 0) {
+		console.log(i + ' - это ноль');
+	} else if ((i % 2) == 0) {
+		console.log(i + ' - это четное число');
+	} else if ((i % 2) != 0) {
+		console.log(i + ' - это нечетное число');
 	}
+
 }
+*/
+
+
+
 
---
+++ js/script6.js
@@ -0,0 +1,8 @@
+'use ctrict'
+
+//Задание 6
+for (let j = "X"; j.length < 21; j = j + "X") {
+
+	console.log(j);
+
+}
